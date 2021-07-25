8caa54ee2869203613198a842d27870b5117bf0e
csherms <casey.sherman@bryanuniversity.edu>
2021-07-24 18:08:07 -0600

    140_Capstone

--- week-6/Full_Stack_SQL/client/src/App.js
+++ week-6/Full_Stack_SQL/client/src/App.js
@@ -1,8 +1,161 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
+import Axios from "axios";
 
 function App() {
-  return <div className='App'></div>;
+  // Use State Hooks
+  const [name, setName] = useState("");
+  const [age, setAge] = useState(0);
+  const [country, setCountry] = useState("");
+  const [position, setPosition] = useState("");
+  const [wage, setWage] = useState(0);
+  const [employeeList, setEmployeeList] = useState([]);
+  const [newWage, setNewWage] = useState(0);
+
+  // -------------------------------------------------------------Functions Start-----------------------------------------------
+  const addEmployee = () => {
+    Axios.post("http://localhost:3001/create", {
+      name: name,
+      age: age,
+      country: country,
+      position: position,
+      wage: wage,
+    }).then(() => {
+      setEmployeeList([
+        ...employeeList,
+        {
+          name: name,
+          age: age,
+          country: country,
+          position: position,
+          wage: wage,
+        },
+      ]);
+    });
+  };
+
+  const getEmployees = () => {
+    Axios.get("http://localhost:3001/employees", {}).then((response) => {
+      setEmployeeList(response.data);
+    });
+  };
+
+  const updateEmployeeWage = (id) => {
+    Axios.put("http://localhost:3001/update", { wage: newWage, id: id }).then(
+      (response) => {
+        setEmployeeList(
+          employeeList.map((val) => {
+            return val.id === id
+              ? {
+                  id: val.id,
+                  name: val.name,
+                  country: val.country,
+                  age: val.age,
+                  position: val.position,
+                  wage: newWage,
+                }
+              : val;
+          })
+        );
+      }
+    );
+  };
+
+  const deleteEmployee = (id) => {
+    Axios.delete(`http://localhost:3001/delete/${id}`).then((response) => {
+      setEmployeeList(
+        employeeList.filter((val) => {
+          return val.id !== id;
+        })
+      );
+    });
+  };
+  //  ----------------------------------------------------Functions End--------------------------------------------------------
+
+  // Form For Getting Employee Data
+  return (
+    <div className='App'>
+      <div className='enter-information'>
+        <label>Name:</label>
+        <input
+          type='text'
+          onChange={(e) => {
+            setName(e.target.value);
+          }}
+        />
+        <label>Age:</label>
+        <input
+          type='number'
+          onChange={(e) => {
+            setAge(e.target.value);
+          }}
+        />
+        <label>Country:</label>
+        <input
+          type='text'
+          onChange={(e) => {
+            setCountry(e.target.value);
+          }}
+        />
+        <label>Position:</label>
+        <input
+          type='text'
+          onChange={(e) => {
+            setPosition(e.target.value);
+          }}
+        />
+        <label>Wage (year)</label>
+        <input
+          type='number'
+          onChange={(e) => {
+            setWage(e.target.value);
+          }}
+        />
+        <br></br>
+        <button onClick={addEmployee}>Add Employee</button>
+        <br></br>
+        <button onClick={getEmployees}>Show Employees</button>
+      </div>
+      <br></br>
+
+      {/* Show Employees */}
+      <div className='employee-list'>
+        {employeeList.map((val, key) => {
+          return (
+            <div className='each-employee'>
+              <div>
+                <h4>Name: {val.name}</h4>
+                <h4>Age: {val.age}</h4>
+                <h4>Country: {val.country}</h4>
+                <h4>Position: {val.position}</h4>
+                <h4>Salary: {val.wage}</h4>
+              </div>
+              <div className='card-controls'>
+                <input
+                  type='text'
+                  placeholder='Edit Salary...'
+                  onChange={(e) => {
+                    setNewWage(e.target.value);
+                  }}
+                />
+                <button onClick={() => updateEmployeeWage(val.id)}>
+                  Update
+                </button>
+
+                <button
+                  onClick={(id) => {
+                    deleteEmployee(val.id);
+                  }}
+                >
+                  Delete
+                </button>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 }
 
 export default App;
--- week-6/Full_Stack_SQL/server/index.js
+++ week-6/Full_Stack_SQL/server/index.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const mysql = require("mysql");
 const app = express();
+const cors = require("cors");
+
+app.use(cors());
+app.use(express.json());
 
 // Database Handshake
 const db = mysql.createConnection({
@@ -17,101 +21,66 @@ db.connect((err) => {
   console.log("MySQL Database Connected Successfully.");
 });
 
-// Create New Database Called full_stack_db
-app.get("/createdb", (req, res) => {
-  let sql = "CREATE DATABASE full_stack_db";
-  db.query(sql, (err, result) => {
-    if (err) {
-      throw err;
+// Create New Employee
+app.post("/create", (req, res) => {
+  const name = req.body.name;
+  const age = req.body.age;
+  const country = req.body.country;
+  const position = req.body.position;
+  const wage = req.body.wage;
+  db.query(
+    "INSERT INTO employees (name, age, country, position, wage) VALUES (?,?,?,?,?)",
+    [name, age, country, position, wage],
+    (err, result) => {
+      if (err) {
+        console.log(err);
+      } else {
+        res.send(result);
+      }
     }
-    console.log("full_stack_db Database Created Successfully!");
+  );
+});
+
+// Get All Employees From Database
+app.get("/employees", (req, res) => {
+  db.query("SELECT * FROM employees", (err, result) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.send(result);
+    }
   });
 });
 
-// Create Table in full_stack_db Database
-app.get("/createtable", (req, res) => {
-  let sql =
-    "CREATE TABLE postings (id INT AUTO_INCREMENT, title VARCHAR(50), message VARCHAR(50), PRIMARY KEY(id));";
-  db.query(sql, (err, result) => {
-    if (err) {
-      throw err;
+// Update Employee Wage
+app.put("/update", (req, res) => {
+  const id = req.body.id;
+  const wage = req.body.wage;
+  db.query(
+    "UPDATE employees SET wage = ? WHERE id = ?",
+    [wage, id],
+    (err, result) => {
+      if (err) {
+        console.log(err);
+      } else {
+        res.send(result);
+      }
     }
-    console.log(result);
-    console.log("postings table created successfully!");
+  );
+});
+
+// Delete Employee
+app.delete("/delete/:id", (req, res) => {
+  const id = req.params.id;
+  db.query("DELETE FROM employees WHERE id = ?", id, (err, result) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.send(result);
+    }
   });
 });
 
-// Insert Record 1
-app.get("/insertrow1", (req, res) => {
-  let post = {
-    title: "First Post",
-    message: "This is my first post",
-  };
-  let sql = "INSERT INTO postings SET ?";
-  db.query(sql, post, (err, result) => {
-    if (err) {
-      throw err;
-    }
-    console.log(result);
-    console.log("First Row Inserted Successfully!");
-  });
-});
-
-// Insert Record 2
-app.get("/insertrow2", (req, res) => {
-  let post = {
-    title: "Second Post",
-    message: "This is my second post",
-  };
-  let sql = "INSERT INTO postings SET ?";
-  db.query(sql, post, (err, result) => {
-    if (err) {
-      throw err;
-    }
-    console.log(result);
-    console.log("Second Row Inserted Successfully!");
-  });
-});
-
-// SELECT Rows Command
-app.get("/getpost/:id", (req, res) => {
-  let sql = `SELECT * FROM postings WHERE id = ${req.params.id}`;
-  db.query(sql, (err, result) => {
-    if (err) {
-      throw err;
-    }
-    console.log(result);
-    console.log("Record Retrieved Successfully!");
-  });
-});
-
-// UPDATE Command
-app.get("/updateposts/:id", (req, res) => {
-  let newTitle = "This is updated title via hardcoded value";
-  let sql = `UPDATE postings SET title = '${newTitle}' WHERE id = ${req.params.id}`;
-  db.query(sql, (err, result) => {
-    if (err) {
-      throw err;
-    }
-    console.log(sql);
-    console.log(result);
-    console.log("UPDATE QUERY Executed Successfully!");
-  });
-});
-
-// DELETE Command
-app.get("/deleteposts/:id", (req, res) => {
-  let sql = `DELETE FROM postings WHERE id = ${req.params.id}`;
-  db.query(sql, (err, result) => {
-    if (err) {
-      throw err;
-    }
-    console.log(result);
-    res.send("DELETE Command Executed Successfully!");
-  });
-});
-
-// Server Listen
 app.listen("3001", () => {
   console.log("Server running on Port 3001");
 });
